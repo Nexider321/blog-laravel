06cd9da98dfbb8a80be93e9d93bca4edd327a850
Vladislavs <Vladikuk948@gmail.com>
2022-11-15 11:54:16 +0200

    Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Home;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,17 +12,23 @@ class HomeController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        //
+        $home = Home::all()->first();
+
+        return view ('home', [
+            'name' => $home->name,
+            'description' => $home->description,
+            'logo' => Storage::url($home->logo),
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
@@ -32,11 +41,28 @@ class HomeController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function store(Request $request)
     {
-        //
+        $name = $request->input('name');
+        $description = $request->input('description');
+
+        $home = new Home;
+        $home->name = $name;
+        $home->description = $description;
+
+        $logo_path = $request->file('logo')->store('logos', 'public');
+        $home->logo = $logo_path;
+
+        $home->save();
+
+        $home = Home::all()->first();
+
+        return view('success', [
+            'name' => $home->name,
+            'description' => $home->description,
+        ]);
     }
 
     /**
